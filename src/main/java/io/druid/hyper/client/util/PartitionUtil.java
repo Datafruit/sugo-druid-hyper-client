92bdabde38f9ad439f11b5572255287ab9d23aad
Huangama <huangama.rr@qq.com>
2017-11-29 14:53:08 +0800

    Fix partition bug.

--- src/main/java/io/druid/hyper/client/util/PartitionUtil.java
+++ src/main/java/io/druid/hyper/client/util/PartitionUtil.java
@@ -1,22 +1,13 @@
 package io.druid.hyper.client.util;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Throwables;
 import com.google.common.hash.HashFunction;
 import com.google.common.hash.Hashing;
 
 public class PartitionUtil {
 
     private static final HashFunction hashFunction = Hashing.murmur3_32();
-    private static final ObjectMapper jsonMapper = new ObjectMapper();
 
     public static int getPartitionNum(Object value, int partitions) {
-        try {
-            return Math.abs(hashFunction.hashBytes(jsonMapper.writeValueAsBytes(value)).asInt()) % partitions;
-        }
-        catch (JsonProcessingException e) {
-            throw Throwables.propagate(e);
-        }
+        return Math.abs(hashFunction.hashBytes(value.toString().getBytes()).asInt()) % partitions;
     }
 }
