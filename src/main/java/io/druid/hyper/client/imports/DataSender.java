fa0c12bf67166765b9c9a336bc4cf9a12fc7fb83
chenyuzhi459 <553673833@qq.com>
2018-03-09 11:21:13 +0800

    check closed status

--- src/main/java/io/druid/hyper/client/imports/DataSender.java
+++ src/main/java/io/druid/hyper/client/imports/DataSender.java
@@ -40,6 +40,7 @@ public class DataSender implements Closeable {
     private Progressable reporter;
     private int updateThreshold;
     private int addThreshold = DEFAULT_SEND_THRESHOLD;
+    private boolean closed = false;
 
     private DataSender(String hmaster, String dataSource, Progressable reporter) {
         this(hmaster, dataSource, DEFAULT_SEND_THRESHOLD, reporter);
@@ -84,7 +85,7 @@ public class DataSender implements Closeable {
             Preconditions.checkNotNull(dataSource, "data source can not be null.");
 
             DataSender sender = senderCache.get(this);
-            if (sender == null) {
+            if (sender == null || sender.isClosed()) {
                 sender = new DataSender(server, dataSource, reporter);
 //                senderCache.putIfAbsent(this, sender);
                 senderCache.put(this, sender);
@@ -403,6 +404,9 @@ public class DataSender implements Closeable {
                 size >= addThreshold;
     }
 
+    public boolean isClosed(){
+        return  this.closed;
+    }
     @Override
     public void close() throws IOException {
         if (cacheFlusher != null) {
@@ -463,6 +467,7 @@ public class DataSender implements Closeable {
                 log.error("Failed to shutdown cacheFlusher during close().", e);
             } finally {
                 cacheFlusher = null;
+                this.closed = true;
             }
         }
     }
